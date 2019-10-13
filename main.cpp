f9564192c90bb2b1b3ac1a63b257ff947fa33a2c
Black_idea <53827851+tanzim721@users.noreply.github.com>
2019-10-13 09:29:59 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
-#include<iostream>
-#include<conio.h>
+#include<bits/stdc++.h>
 using namespace std;
-template <class myTemplate1,class myTemplate2>
-myTemplate1 add(myTemplate1 a,myTemplate2 b)
-{
-    return a+b;
-}
-
+typedef pair<int ,int>PII;
+vector<string >vertexName;
+map<string,int>vertexNumbers;
 int main()
 {
-    cout<<add(10.40,30.89)<<endl;
+
+    freopen("topology.txt","r",stdin);
+    int vertex,edge;
+    cin>>vertex>>edge;
+    vector<PII>v[vertex];
+
+    for(int i=0;i<vertex;i++)
+    {
+
+        string str;
+        cin>>str;
+        vertexName.push_back(str);
+        vertexNumbers[str] = i;
+
+    }
+    for(int i=0;i<edge;i++)
+    {
+        string str1,str2;
+        cin>>str1>>str2;
+        int source;
+        PII p;
+        source = vertexNumbers[str1];
+        p.first = vertexNumbers[str2];
+        cin>>p.second;
+        v[source].push_back(p);
+
+    }
+    for(int i=0;i<vertex;i++)
+    {
+        cout<<vertexName[i]<<"--";
+        for(int j=0;j<v[i].size();j++)
+        {
+            cout<<vertexName[v[i][j].first]<<"-"<<v[i][j].second<<"-";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
     return 0;
 }
